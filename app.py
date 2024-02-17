70084231c0a13520513935c026c9103de6a08093
Wolt90 <gleb90@list.ru>
2024-02-17 03:25:10 +0300

    v 0.2

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import pandas as pd
-from flask_sqlalchemy import SQLAlchemy
 import time
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, FileField, DateField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///competitions.db'
 app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
 
@@ -17,12 +19,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-
-class NewsForm(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired()])
-    # image = 
-    date = TextAreaField('Дата', validators=[DataRequired()])
-    submit = SubmitField('Сохранить')
 
 # Страница регистрации
 @app.route('/register', methods=['GET', 'POST'])
@@ -49,44 +45,40 @@ def success():
     # time.sleep(5)
     return 'Регистрация прошла успешно!' #redirect('/')
 
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False, default='default.jpg')
+    date = db.Column(db.Date, nullable=False)
+
+class CompetitionForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    image = FileField('Картинка', validators=[DataRequired()])
+    date = DateField('Дата', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    competitions = Competition.query.all()
+    return render_template('index.html', competitions=competitions)
 
-@app.route('/page1')
-def page1():
-    # Генерируем данные для таблицы
-    data = [
-        ['Value 1', 'Value 2', 'Value 3'],
-        ['Value 4', 'Value 5', 'Value 6'],
-        ['Value 7', 'Value 8', 'Value 9']
-    ]
-    return render_template('page1.html', data=data)
-
-# Другие маршруты для остальных страниц
-
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
-
-@app.route('/page3')
-def page3():
-    return render_template('page3.html')
-
-@app.route('/page4')
-def page4():
-    return render_template('page4.html')
-
-@app.route('/page5')
-def page5():
-    return render_template('page5.html')
-
-@app.route('/page6')
-def page6():
-    return render_template('page6.html')
+@app.route('/add_competition', methods=['GET', 'POST'])
+def add_competition():
+    form = CompetitionForm()
+    if form.validate_on_submit():
+        competition = Competition(title=form.title.data, date=form.date.data)
+        if form.image.data:
+            filename = form.image.data.filename
+            form.image.data.save(os.path.join('static', 'images', filename))
+            competition.image = filename
+        db.session.add(competition)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_competition.html', form=form)
 
 with app.app_context(): 
     db.create_all() 
 
 if __name__ == '__main__':
+    # db.create_all()
     app.run(debug=True)
